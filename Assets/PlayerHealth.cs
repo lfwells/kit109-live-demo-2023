f892cda18499a639d326ec8bd43a9f9b10c40391
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-09-16 01:01:42 +1000

    Converted the PlayerHealth script to use the FloatVal scriptable objects we just made

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float health = 10;
-    public float maxHealth = 10;
+    public FloatVal health;
+    public FloatVal maxHealth;
 
     void OnGUI()
     {
-        GUI.TextArea(new Rect(10, 10, 100, 20), "Health: " + health + " / "+maxHealth);   
+        GUI.TextArea(new Rect(10, 10, 100, 20), "Health: " + health + " / "+maxHealth);
+        //note that we can use health as a float here, because of the "magic" in FloatVal.cs
+        //without the magic, we could have written the above line as:
+        //GUI.TextArea(new Rect(10, 10, 100, 20), "Health: " + health.Value + " / " + maxHealth.Value);
     }
 }
