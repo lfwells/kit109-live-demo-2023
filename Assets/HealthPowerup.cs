dfb3004699ca4ce9b91f25ad8f0ca202cb9b9c5f
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-09-16 01:02:18 +1000

    Updated the health powerup code to use the new floatval for max health

--- Assets/HealthPowerup.cs
+++ Assets/HealthPowerup.cs
@@ -9,6 +9,8 @@ public class HealthPowerup : Powerup
     
     public override void ApplyPowerup(GameObject player)
     {
-        player.GetComponent<PlayerHealth>().maxHealth += healthAmount;
+        player.GetComponent<PlayerHealth>().maxHealth.Value += healthAmount;
+        //note that we needed to use .Value above, because maxHealth is a FloatVal
+        //this is a slight downside to this extended approach, as the code is a bit more verbose/ugly
     }
 }
